3aca24be9c0ea4dd38d9b5818faa2d22818dcd74
sebastian461 <sebastiano-461@hotmail.com>
2023-06-18 18:21:46 -0500

    Layout refactor
    Restructured the design into smaller components

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DBZMainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [DBZMainPageComponent],
+  declarations: [DBZMainPageComponent, ListComponent, FormComponent],
   exports: [DBZMainPageComponent],
   imports: [CommonModule],
 })
