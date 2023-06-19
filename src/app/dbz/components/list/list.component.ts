e32138b30da59a70b641e48deb3d49d08f05258b
sebastian461 <sebastiano-461@hotmail.com>
2023-06-18 19:25:52 -0500

    ngFor and @Input
    Expansion of ngFor and use of interfaces between parent and child components

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  /* 
+  Esto es un decorador que le dice al componente hijo que va a recibir algo del componente padre
+  En caso de no recibir algo se puede definir el valor por defecto
+  */
+  @Input()
+  public characterList: Character[] = [];
+}
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -3,8 +3,29 @@ Este es un componente principal que agrupara al resto de componentes
 */
 import { Component } from '@angular/core';
 
+import { Character } from '../interfaces/character.interface';
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
-export class DBZMainPageComponent {}
+export class DBZMainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 10000,
+    },
+    {
+      name: 'Vegeta',
+      power: 9000,
+    },
+    {
+      name: 'Piccolo',
+      power: 8000,
+    },
+    {
+      name: 'Nappa',
+      power: 7000,
+    },
+  ];
+}
